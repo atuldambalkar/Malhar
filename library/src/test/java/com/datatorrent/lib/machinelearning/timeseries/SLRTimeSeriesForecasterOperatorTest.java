51054e38be49660978ebf96d13c5ee8f15f97e9b
Atul Dambalkar <atul@datatorrent.com>
2014-01-31 16:03:11 +0530

    #MLHR-678 #comment Added ASL notices at the top of the file.

--- library/src/test/java/com/datatorrent/lib/machinelearning/timeseries/SLRTimeSeriesForecasterOperatorTest.java
+++ library/src/test/java/com/datatorrent/lib/machinelearning/timeseries/SLRTimeSeriesForecasterOperatorTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2013 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.datatorrent.lib.machinelearning.timeseries;
 
 import com.datatorrent.lib.machinelearning.timeseries.linearregression.SLRTimeSeries;
@@ -59,7 +74,5 @@ public class SLRTimeSeriesForecasterOperatorTest {
         Assert.assertEquals(8.57, (Double)tuples.get(2), 0.01);
         Assert.assertEquals(9.19, (Double)tuples.get(3), 0.01);
         Assert.assertEquals(9.74, (Double)tuples.get(4), 0.01);
-
-
     }
 }
