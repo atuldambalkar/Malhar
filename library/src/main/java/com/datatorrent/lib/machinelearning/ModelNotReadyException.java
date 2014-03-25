2d19b712acbd66159c5197c7cf89103d4dcc56b5
Atul Dambalkar <atul@datatorrent.com>
2014-03-25 17:51:47 +0530

    #MLHR-726 #comment Removed unused constructors.

--- library/src/main/java/com/datatorrent/lib/machinelearning/ModelNotReadyException.java
+++ library/src/main/java/com/datatorrent/lib/machinelearning/ModelNotReadyException.java
@@ -11,16 +11,4 @@ public class ModelNotReadyException extends  Exception {
     public ModelNotReadyException(String message) {
         super(message);
     }
-
-    public ModelNotReadyException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public ModelNotReadyException(Throwable cause) {
-        super(cause);
-    }
-
-    public ModelNotReadyException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
